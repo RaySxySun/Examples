09d12a29498acf09dab12bedc66000d32a62781f
ASNPHSG <ohraymaster@gmail.com>
2018-01-08 16:23:08 +0800

    design pattern

--- src/main/java/datastructure/tree/rbTree/RBTree.java
+++ src/main/java/datastructure/tree/rbTree/RBTree.java
@@ -1,8 +1,8 @@
-package algorithm.search.rbTree;
+package datastructure.tree.rbTree;
 
 // https://www.cnblogs.com/skywang12345/p/3624343.html
-
-import com.sun.org.apache.regexp.internal.RE;
+// http://blog.jobbole.com/70639/
+// http://blog.jobbole.com/61872/#toc3
 
 /*
  * RBTree: like a binary tree. Max Height for N nodes tree is 2log(n + 1)
@@ -249,5 +249,97 @@ public class RBTree <T extends Comparable<T>> {
             }
         }
         setBlack(this.mRoot);
+    }
+
+    private void remove(RBTNode<T> node) {
+        RBTNode<T> child, parent;
+        boolean color;
+
+        if ((node.left != null) && (node.right != null)) {
+            RBTNode<T> replace = node;
+
+            replace = replace.right;
+            while (replace.left != null) {
+                replace = replace.left;
+            }
+
+            if (parentOf(node) != null) {
+                if (parentOf(node).left == node) {
+                    parentOf(node).left = replace;
+                } else {
+                    parentOf(node).right = replace;
+                }
+            } else {
+                this.mRoot = replace;
+            }
+
+            child = replace.right;
+            parent = parentOf(replace);
+
+            color = colorOf(replace);
+
+            if (parent == node) {
+                parent = replace;
+            } else {
+                if (child != null) {
+                    setParent(child, parent);
+                }
+                parent.left = child;
+                replace.right = node.right;
+                setParent(node.right, replace);
+            }
+            replace.parent = node.parent;
+            replace.color = node.color;
+            replace.left = node.left;
+            node.left.parent = replace;
+
+            if (color == BLACK) {
+                removeFixUp(child, parent);
+            }
+
+            node = null;
+            return;
+        }
+
+        if (node.left != null) {
+            child = node.left;
+        } else {
+            child = node.right;
+        }
+
+        parent = node.parent;
+        color = node.color;
+        if (child != null) {
+            child.parent = parent;
+        }
+
+        if (parent != null) {
+            if (parent.left == node) {
+                parent.left = child;
+            } else {
+                parent.right = child;
+            }
+        } else {
+            this.mRoot = child;
+        }
+
+        if (color == BLACK) {
+            removeFixUp(child, parent);
+        }
+        node = null;
+    }
+
+    public void remove(T key) {
+        RBTNode<T> node;
+        if ((node = search(mRoot, key)) != null) {
+            remove(node);
+        }
+    }
+
+    private void removeFixUp(RBTNode<T> child, RBTNode<T> parent) {
+    }
+
+    private RBTNode<T> search(RBTNode<T> mRoot, T key) {
+        return null;
     }
 }
