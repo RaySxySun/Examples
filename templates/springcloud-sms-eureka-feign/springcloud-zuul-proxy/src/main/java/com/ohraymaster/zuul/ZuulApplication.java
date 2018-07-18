462a39bb31126f4c50832edeb8582893bc3e9c18
ohraymaster <ohraymaster@gmail.com>
2018-07-18 18:15:03 +0800

    add zuul proxy

---
+++ templates/springcloud-sms-eureka-feign/springcloud-zuul-proxy/src/main/java/com/ohraymaster/zuul/ZuulApplication.java
@@ -0,0 +1,18 @@
+package com.ohraymaster.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * Created by ray on 18-7-18.
+ */
+@EnableZuulProxy
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ZuulApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class, args);
+    }
+}
